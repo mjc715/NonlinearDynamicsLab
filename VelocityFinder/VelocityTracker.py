50435c69a51064d7bf058ba66dd97b2ea9cc78aa
Michael Castellucci <91853114+mjc715@users.noreply.github.com>
2023-04-02 20:14:35 -0400

    Update VelocityTracker.py

--- VelocityFinder/VelocityTracker.py
+++ VelocityFinder/VelocityTracker.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 import sys
 import numpy as np
 
-measuring_length = .30  # meters
+# --- Adjust these variables as needed for each video
+measuring_length = 2.35  # meters
 camera_fps = 30  # frames per second
-video_path = "ANMR0010.mp4"
-numPoints = 10  # number of times velocity will be calculated
-tracker_type = 'MIL'
-releaseFrame = 10  # this is the frame where the obj is released and the timer starts
+releaseFrame = 160  # this is the frame where the obj is released and the timer starts
+numPoints = 20  # number of times velocity will be calculated
+video_path = "Wave Tank Vids\ANMR0029.mp4"
+
+tracker_type = 'CSRT'
 video = cv2.VideoCapture(video_path)
+
+# --- Parameters for calculating delta if desired
 FLUID_VISCOSITY = 8.927 * 10**-7  # kinematic viscosity m^2/s
 FLUID_DENSITY = 997  # kg/m^3
 SPHERE_RADIUS = .02  # meters
@@ -32,30 +36,30 @@ frame = cv2.resize(frame, (854, 480))
 measuringBounds = cv2.selectROI(
     "Select tracking area", frame)  # Start area at top of ball
 cv2.destroyAllWindows()
-pixel_length = measuringBounds[3]  # gets pixel height of selection
+# pixel_length = measuringBounds[3]  # gets pixel height of selection VERTICAL
+pixel_length = measuringBounds[2]  # HORIZONTAL
 PIXELS_PER_METER = pixel_length / measuring_length
-trackingPoints, interval = np.linspace(measuringBounds[1],
-                                       measuringBounds[1] + measuringBounds[3], numPoints, retstep=True)  # returns various heights to get velocity at
-
+# trackingPoints, interval = np.linspace(measuringBounds[1],
+#                                        measuringBounds[1] + measuringBounds[3], numPoints, retstep=True)  # returns various heights to get velocity at (VERTICAL)
+trackingPoints, interval = np.linspace(measuringBounds[0],
+                                       measuringBounds[0] + measuringBounds[2], numPoints, retstep=True)  # returns various points to get velocity at (HORIZONTAL)
 
 params = cv2.SimpleBlobDetector_Params()
-params.filterByCircularity = False
+params.filterByCircularity = True
 params.filterByColor = True
 params.blobColor = 150
-params.minCircularity = 0.9
+params.minCircularity = .8
 
 if int(7) < 3:
     tracker = cv2.Tracker_create(tracker_type)
 else:
-    tracker = cv2.TrackerMIL_create()
+    tracker = cv2.TrackerCSRT_create()
 
-# --- Creating foreground mask
+
 frame = cv2.resize(frame, (854, 480))
 if not rete:
     print('Cant read video file')
     sys.exit()
-# BS_KNN = cv2.createBackgroundSubtractorKNN()
-# frame = BS_KNN.apply(frame)
 
 objectBounds = cv2.selectROI("Select object", frame)
 cv2.destroyAllWindows()
@@ -63,12 +67,12 @@ rete = tracker.init(frame, objectBounds)
 p1, prev = None, None
 
 frameCount, prevFrame, speed = releaseFrame, 0, 0
-trackingCount = numPoints - 2  # Points start at where the ball is so skip one point
+# trackingCount = numPoints # VERTICAL
+trackingCount = 0  # HORIZONTAL
 velocities, times = [], []
 
 while video.isOpened():
     rete, frame = video.read()
-    # frame = BS_KNN.apply(frame)
     frameCount += 1
 
     if not rete:
@@ -90,7 +94,8 @@ while video.isOpened():
         cv2.putText(frame, "Tracking failure detected", (100, 80),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
-    if objectBounds[1] <= trackingPoints[trackingCount] and trackingCount >= 0:
+    if trackingCount <= numPoints - 1 and objectBounds[0] >= trackingPoints[trackingCount]:
+        # if objectBounds[1] <= trackingPoints[trackingCount] and trackingCount >= 0: # VERTICAL
         timeInterval = (frameCount - prevFrame) / camera_fps
         length = interval / PIXELS_PER_METER
         speed = length / timeInterval
@@ -98,7 +103,8 @@ while video.isOpened():
         totalTime = (frameCount - releaseFrame) / camera_fps
         times.append(totalTime)
         prevFrame = frameCount
-        trackingCount -= 1
+        # - for VERTICAL, + for HORIZONTAL, also change trackingCount to start at 0
+        trackingCount += 1
 
     cv2.putText(frame, "Velocity : " + str(float(speed)), (100, 100),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
@@ -114,7 +120,16 @@ while video.isOpened():
 video.release()
 cv2.destroyAllWindows()
 
-# Formatting the plot
+# This removes the points in the first half second
+i = 0
+t = 0
+while t <= .5 and i < numPoints-1:
+    t = times[i]
+    i += 1
+times = times[i:]
+velocities = velocities[i:]
+
+# Formatting plots
 fig, ax = plt.subplots()
 ax.plot(times, velocities, 'bo')
 ax.set_xlabel('time (s)')
@@ -127,13 +142,13 @@ plt.show()
 # Getting avg velocity and using it to calculate delta
 vAvg = np.sum(velocities) / len(velocities)
 print("\n\n")
-str = "Average vertical velocity: {} m/s"
+str = "Average velocity: {} m/s"
 print(str.format(vAvg))
-EQ_CONSTANT = (3/2)*((3 * FLUID_VISCOSITY * vAvg) /
-                     (SPHERE_RADIUS**2 * FLUID_DENSITY))
-a = 2*GRAVITY
-b = a - EQ_CONSTANT
-c = 2 * EQ_CONSTANT
-deltas = np.roots([a, -b, -c])
-str = "Delta value: {} \n\n"
-print(str.format(deltas[0]))
+# EQ_CONSTANT = (3/2)*((3 * FLUID_VISCOSITY * vAvg) /
+#                      (SPHERE_RADIUS**2 * FLUID_DENSITY))
+# a = 2*GRAVITY
+# b = a - EQ_CONSTANT
+# c = 2 * EQ_CONSTANT
+# deltas = np.roots([a, -b, -c])
+# str = "Delta value: {} \n\n"
+# print(str.format(deltas[0]))
