2044b77f06277a6cc70196f9f4fea1dbca82eb9a
Michael Castellucci <mjc715@miami.edu>
2023-07-26 13:05:41 -0400

    updated

--- Machine Learning/practice.jl
+++ Machine Learning/practice.jl
@@ -3,28 +3,58 @@ Pkg.add("DiffEqParamEstim")
 Pkg.add("DifferentialEquations")
 Pkg.add("Plots")
 Pkg.add("RecursiveArrayTools")
-using DiffEqParamEstim, DifferentialEquations, Plots
+Pkg.add("Optimization")
+Pkg.add("OptimizationOptimJL")
+Pkg.add("ForwardDiff")
+Pkg.add("OptimizationBBO")
+Pkg.add("Optim")
 
-f(x, p, t) = p * x
-x_start = 10.0
-p = 1.0
-tspan = (0.0, 1.0)
-prob = ODEProblem(f, x_start, tspan, p)
-sol = solve(prob)
+using DiffEqParamEstim, DifferentialEquations, Plots, Optimization, OptimizationOptimJL
+using RecursiveArrayTools, ForwardDiff, OptimizationBBO, Optim
 
-#??????????????????????????????????????????????????
-
-using RecursiveArrayTools # for VectorOfArray
-t = collect(range(0, stop=10, length=200))
-function generate_data(t)
-    sol = solve(prob)
-    randomized = VectorOfArray([(sol(t[i]) + 0.01randn(2)) for i in 1:length(t)])
-    data = convert(Array, randomized)
+#--- Define the diff eq p = params
+function f(u, p, t)
+    du = p[1] * u
+    return du
 end
-aggregate_data = convert(Array, VectorOfArray([generate_data(t) for i in 1:10000]))
-monte_prob = EnsembleProblem(prob)
-obj = build_loss_objective(monte_prob, L2Loss(t, aggregate_data), Optimization.AutoForwardDiff(),
-    maxiters=10000, verbose=false, trajectories=1000)
-optprob = Optimization.OptimizationProblem(obj, [1.0, 0.5])
-result = solve(optprob, Optim.BFGS())
-println(result)
+
+#--- Parameters for the DE
+x_start = 1.0
+p = [3.0]
+tspan = (0.0, 10.0)
+#--- Define problem and solve
+prob = ODEProblem(f, x_start, tspan, p)
+sol = solve(prob, Tsit5())
+#--- Get times to test at
+t = collect(range(0, stop=10, length=200))
+#--- Getting noisy data points to reverse engineer params with
+randomized = VectorOfArray([(sol(t[i]) .+ 0.01randn(2)) for i in 1:length(t)])
+data = convert(Array, randomized)
+
+# println(data)
+# newprob = remake(prob, p=0.9)
+# newsol = solve(newprob, Tsit5())
+# plot(sol)
+# display(plot!(newsol))
+# sleep(10)
+
+#--- What ML optimizes to get DE
+cost_function = build_loss_objective(prob, Tsit5(), L2Loss(t, data),
+    Optimization.AutoForwardDiff(),
+    maxiters=10000, verbose=false)
+
+# vals = 0.0:0.1:5.0
+# display(plot(vals, [cost_function(i) for i in vals], yscale=:log10,
+#     xaxis="Parameter", yaxis="Cost", title="1-Parameter Cost Function",
+#     lw=3))
+# sleep(10)
+
+#--- Optimizing loss function and using result as parameter for DE
+optprob = Optimization.OptimizationProblem(cost_function, [0.9])
+optsol = solve(optprob, BFGS())
+newprob = remake(prob, p=optsol.u)
+newsol = solve(newprob, Tsit5())
+plot(sol)
+display(plot!(newsol))
+println(optsol.u)
+sleep(10)
---
+++ Machine Learning/tempCodeRunnerFile.jl
@@ -0,0 +1,2 @@
+newprob = remake(prob, p=0.9)
+# newsol = solve(newprob, Tsit5())
