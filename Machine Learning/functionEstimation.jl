6a43672472b57047d84271660c0a8d352fb3a8eb
Michael Castellucci <mjc715@miami.edu>
2023-08-04 10:49:26 -0400

    creation

---
+++ Machine Learning/functionEstimation.jl
@@ -0,0 +1,23 @@
+using DataDrivenDiffEq, ModelingToolkit, OrdinaryDiffEq
+using DataDrivenSparse, LinearAlgebra
+
+function f(u, p, t)
+    du = 2.0 * u
+    return du
+end
+
+u0 = [2.0]
+tspan = (0.0, 10.0)
+dt = 0.1
+prob = ODEProblem(f, u0, tspan)
+sol = solve(prob, Tsit5(), saveat=dt)
+
+ddprob = DataDrivenProblem(sol)
+
+@variables t x(t)
+u = [x]
+basis = Basis(polynomial_basis(u, 5), u, iv=t)
+# Not working
+opt = STLSQ(exp10.(-5:0.1:-1))
+ddsol = solve(ddprob, basis, opt, options=DataDrivenCommonOptions(digits=1))
+println(get_basis(ddsol))
