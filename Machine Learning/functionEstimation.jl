2ed45d25d8f5ca7d9af42e0738ce4df92cc5d832
Michael Castellucci <mjc715@miami.edu>
2023-08-08 17:48:23 -0400

    can now use defined data

--- Machine Learning/functionEstimation.jl
+++ Machine Learning/functionEstimation.jl
@@ -1,5 +1,7 @@
 using DataDrivenDiffEq, ModelingToolkit, OrdinaryDiffEq
-using DataDrivenSparse, LinearAlgebra
+using DataDrivenSparse, LinearAlgebra, StableRNGs, Plots
+
+rng = StableRNG(1000)
 
 function f(u, p, t)
     x = 2.0 * u[1]
@@ -7,16 +9,45 @@ function f(u, p, t)
 end
 
 u0 = [1.0]
-tspan = (0.0, 10.0)
+tspan = (0.0, 3.0)
 dt = 0.1
 prob = ODEProblem(f, u0, tspan)
 sol = solve(prob, Tsit5(), saveat=dt)
 
-ddprob = DataDrivenProblem(sol)
+X = sol[:, :] + 0.2 .* randn(rng, size(sol)) # Making random data
+ts = sol.t
 
-@variables t x(t)
-u = [x]
-basis = Basis(polynomial_basis(u, 5), u, iv=t)
-opt = STLSQ(exp10.(-5:0.1:-1))
-ddsol = solve(ddprob, basis, opt, options=DataDrivenCommonOptions(digits=1)) # Not working
-println(get_basis(ddsol))
+prob = ContinuousDataDrivenProblem(X, ts, GaussianKernel(),
+    U=(u, p, t) -> [2 * t]) # What does this part specify?
+
+@variables u[1:1] c[1:1]
+@parameters w[1:2]
+u = collect(u)  # why the extra variables?
+w = collect(w)
+c = collect(c)
+
+h = Num[polynomial_basis(u, 5); c]
+basis = Basis(h, u, parameters=w, controls=c)
+
+sampler = DataProcessing(split=0.8, shuffle=true, batchsize=25, rng=rng)
+lambdas = exp10.(-10:0.1:0)
+opt = STLSQ(lambdas) # How does the optimization work?
+res = solve(prob, basis, opt, options=DataDrivenCommonOptions(data_processing=sampler, digits=1))
+system = get_basis(res)
+params = get_parameter_map(system)
+println(system)
+println(params)
+display(plot(plot(prob), plot(res), layout=(1, 2)))
+sleep(10)
+
+
+
+# Find function without random dataset:
+# 
+# ddprob = DataDrivenProblem(sol)
+# @variables t x(t)
+# u = [x]
+# basis = Basis(polynomial_basis(u, 5), u, iv=t)
+# opt = STLSQ(exp10.(-5:0.1:-1))
+# ddsol = solve(ddprob, basis, opt, options=DataDrivenCommonOptions(digits=1)) # Not working
+# println(get_basis(ddsol))
