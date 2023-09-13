aefe2d37671945422d5bcd4e14083eba394a546f
Michael Castellucci <mjc715@miami.edu>
2023-09-13 15:11:40 -0400

    updated parameters for equation prediction

--- Machine Learning/functionEstimation.jl
+++ Machine Learning/functionEstimation.jl
@@ -3,6 +3,9 @@ using DataDrivenSparse, LinearAlgebra, StableRNGs, Plots
 
 rng = StableRNG(1000)
 
+# Ctrl-Shift-P -> Start REPL -> pwd() -> cd("Machine Learning") -> include("____")
+# Work on preventing overfitting
+
 function f(u, p, t)
     x, y = u
     dx = 2.0 * x * y
@@ -11,8 +14,8 @@ function f(u, p, t)
 end
 
 u0 = [1.0; 0]
-tspan = (0.0, 1.0)
-dt = 0.1
+tspan = (0.0, 2.0)
+dt = 0.0001
 prob = ODEProblem(f, u0, tspan)
 sol = solve(prob, Tsit5(), saveat=dt)
 
@@ -24,20 +27,19 @@ prob = ContinuousDataDrivenProblem(X, ts, GaussianKernel(),) # What does this pa
 @variables u[1:2]
 u = collect(u)
 
-h = Num[polynomial_basis(u, 5); u]
+h = Num[polynomial_basis(u, 2); u]
 basis = Basis(h, u)
-# basis = Basis(h)
 
 sampler = DataProcessing(split=0.8, shuffle=true, batchsize=25, rng=rng)
 lambdas = exp10.(-10:0.1:0)
 opt = STLSQ(lambdas) # How does the optimization work?
-res = solve(prob, basis, opt, options=DataDrivenCommonOptions(data_processing=sampler, digits=1))
+res = solve(prob, basis, opt, options=DataDrivenCommonOptions(data_processing=sampler, digits=2))
 system = get_basis(res)
 params = get_parameter_map(system)
 println(system)
 println(params)
 display(plot(plot(prob), plot(res), layout=(1, 2)))
-sleep(10)
+# sleep(10)
 
 
 
